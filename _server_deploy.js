4231e0561ff726b4972ae73451debc77fb81ac46
Raiyan109 <kraiyan109@gmail.com>
2022-04-27 18:21:16 +0600

    first commit

---
+++ _server_deploy.js
@@ -0,0 +1,15 @@
+/**
+ * ----------------------------
+ * One Time for your computer
+ * ---------------------------
+ * 1. create Heroku account
+ * 2. verify email
+ * 3. install heroku CLI
+ * 4. heroku login
+ * 
+ * --------------------
+ * For each project one time 
+ *  -----------------------------
+ * 1. heroku create
+ * 2. 
+ */
